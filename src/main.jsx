ecd0cd53483b5b9d649bda9579e42211bebb4867
chen <peter-chen@rantion.com>
2022-03-28 11:31:45 +0800

    feature: add font

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./App.css"
 import { BrowserRouter,Routes, Route } from "react-router-dom";
+import "./App.css"
 import './index.css'
 import 'antd/dist/antd.css';
+import './assets/font.css';
 import Task1 from "./views/task1/index.jsx";
 import TigerBot from "./views/tigerBot/index.jsx";
 import zhCN from 'antd/lib/locale/zh_CN';
